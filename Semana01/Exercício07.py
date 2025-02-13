620e36a1392619a942b01beaa380865e31b02166
Pedro <pedrocalezanelyra@gmail.com>
2025-02-13 17:37:56 -0300

    Adicionado exercício 7: cálculo do troco de uma compra

---
+++ Semana01/Exercício07.py
@@ -0,0 +1,6 @@
+VC = float(input("Insira o custo: "))      # recebe o valor da compra
+VP = float(input("Insira o valor pago: ")) # recebe o valor pago
+
+troco = VP - VC                            # cálculo do troco
+
+print("Troco da compra: ", troco)          # exibição do troco

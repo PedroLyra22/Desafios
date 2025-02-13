b4e5732c2c0537ddefdfbdfb2cf51256a3213462
Pedro <pedrocalezanelyra@gmail.com>
2025-02-13 19:50:21 -0300

    Adicionado exercício 11: cálculo de raíz utilizando a biblioteca math

---
+++ Semana01/exercicio_11.py
@@ -0,0 +1,7 @@
+import math
+
+usuarios = int(input("Informe a quantidade de usuários: "))
+
+usuarios_raiz = math.isqrt(usuarios)
+
+print("A raíz quadrada dos seu número de funcionários é: ", usuarios_raiz)

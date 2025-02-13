6f3c91cba024a206f810d6c09c8c6a167f74b984
Pedro <pedrocalezanelyra@gmail.com>
2025-02-13 19:17:03 -0300

    Adicionado exercício extra 1: trabalho com if else

---
+++ Semana01/exercicio_extra_1.py
@@ -0,0 +1,7 @@
+numero_1 = int(input("Digite um número: "))
+numero_2 = int(input("Digite outro número: "))
+
+if (numero_1 > numero_2):
+    print("O maior número é: ",numero_1)
+else:
+    print("O maior número é: ", numero_2)

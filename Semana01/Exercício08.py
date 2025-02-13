e4d25103ddb17c0d17aee6e26492f9d74db29f32
Pedro <pedrocalezanelyra@gmail.com>
2025-02-13 17:43:48 -0300

    Adicionado exercício 8: cálculo da divisão inteira e do resto

---
+++ Semana01/Exercício08.py
@@ -0,0 +1,8 @@
+x = int (input("Digite um número: ")) # solicitação do primeiro número
+y = int (input("Digite um número: ")) # solicitação do segundo  número
+
+div = x // y
+resto = x % y
+
+print("A divisão interia entre os números é: ", div)
+print("O resto da divisão entre os número é: ", resto)

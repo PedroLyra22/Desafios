1bb154a2083d92f514287e13d29a44066e87acbd
Pedro <pedrocalezanelyra@gmail.com>
2025-02-13 17:27:50 -0300

    Adicionado exercício 6: cálculo do quadrado de um número

---
+++ Semana01/Exercício06.py
@@ -0,0 +1,5 @@
+x = int (input("Digite um número: "))     # solicitação de um número qualquer
+
+pot = x ** 2                              # cálculo do quadrado do número inserido
+
+print("Esse número ao quadrado é: ", pot) # impressão do resultado da potência

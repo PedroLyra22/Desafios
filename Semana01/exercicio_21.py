dffba9620fc576d33df6b1a2cf8e88c66136e01f
Pedro <pedrocalezanelyra@gmail.com>
2025-02-16 14:23:08 -0300

    Adicionado exercício 21: média do peso de 3 passageiros

---
+++ Semana01/exercicio_21.py
@@ -0,0 +1,7 @@
+peso_1 = float(input("Digite o peso do primeiro passageiro: "))
+peso_2 = float(input("Digite o peso do segundo passageiro: "))
+peso_3 = float(input("Digite o peso do terceiro passageiro: "))
+
+media = (peso_3 + peso_2 + peso_1) / 3
+
+print("A média do peso dos passageiro é: ", media)
